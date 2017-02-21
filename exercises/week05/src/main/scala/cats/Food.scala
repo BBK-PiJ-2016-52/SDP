18bd89af4d5f06bed8e3bd5338cadf8d5902cfbe
BBK-PiJ-2016-52 <UrbanTerror41>
2017-02-21 16:12:19 +0000

    Reorganazing Repository.

--- exercises/week05/src/main/scala/cats/Food.scala
+++
@@ -1,7 +0,0 @@
-package cats
-
-sealed trait Food
-final case object Antelope extends Food
-final case object TigerFood extends Food
-final case object Licorice extends Food
-final case class Mice() extends Food
--- exercises/week07/build.sbt
+++
@@ -1,6 +0,0 @@
-name := "week07"
-
-version := "1.0"
-
-scalaVersion := "2.12.1"
-    

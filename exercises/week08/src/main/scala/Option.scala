79823e39b871b26d515c8853bf2b04bacfafed72
BBK-PiJ-2016-52 <UrbanTerror41>
2017-02-28 16:01:34 +0000

    Adding example week08

---
+++ exercises/week08/src/main/scala/Option.scala
@@ -0,0 +1,14 @@
+
+sealed trait Option[A+] {
+   def getOrElse(default: A): A
+   def isEmpty: Boolean
+   def isDefined: Boolean = !isEmpty
+   // whole lot of other methods..
+}
+final case class Some[A](x:A) extends Option[A]{
+   def getOrElse(default: A) = x
+   def isEmpty = false
+   // other methods
+}
+
+final case

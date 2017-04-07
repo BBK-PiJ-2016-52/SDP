f6bd3f19b414082e898aba519015d2a0845259a3
Eric Rodriguez <erodri07@mail.bbk.ac.uk>
2017-04-07 11:09:33 +0100

    Completed Exercise Sheet 01 - Week 01.

--- exercises/week01/src/ex01.scala
+++ exercises/week01/src/ex01.scala
@@ -5,7 +5,7 @@ object ex01 {
   def main(args: Array[String]) {
 
     // Get started:
-    var a = 2 + 2
+    val a = 2 + 2
     println ("The result of 2 + 2 = " + a)
 
     // Reading and printing:
@@ -25,19 +25,24 @@ object ex01 {
     // Use a while loop:
     println("Type 1 to start, 0 to finish.")
     var numberRegistered = readLine.toInt
-    while(numberRegistered != 0){
+    while(numberRegistered == 0 || numberRegistered <= 100){
       println("Enter a number to obtain the square: ")
       numberRegistered = readLine.toInt
       println (numberRegistered*numberRegistered)
     }
 
     // Use a for loop:
-    var i = 0;
     for( i <- 1 to 25){
-      println( "Value of i is: " + i + " the square is: " + (i*i) + " and the cube Square is: " + scala.math.pow(i,3) );
+      println( "Value of i is: " + i + " the square is: " + (i*i) + " and the cube Square is: " + scala.math.pow(i,3) )
     }
 
-    
+    // Writting a Function
+    def isEven(num: Int): AnyVal = {
+      if (num % 2 == 0) true
+    }
 
+    println("The number 2 is even: " + isEven(2))
+    println("The number 3 is even: " + isEven(2))
+    println("The number 22 is even: " + isEven(2))
   }
 }

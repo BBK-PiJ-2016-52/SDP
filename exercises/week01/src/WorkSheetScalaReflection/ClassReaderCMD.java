e259713c40b1d792e18e25d8c512fcaaa796e161
Eric Rodriguez <erodri07@mail.bbk.ac.uk>
2017-04-25 12:54:01 +0100

    Added additional modifiers and types for better implementation.

--- exercises/week01/src/WorkSheetScalaReflection/ClassReaderCMD.java
+++ exercises/week01/src/WorkSheetScalaReflection/ClassReaderCMD.java
@@ -5,23 +5,25 @@ import java.util.Scanner;
 /**
  * Created by Eric on 15/04/2017.
  */
-public class Reflection {
+public class ClassReaderCMD {
 
     public static void main(String[] args) throws ClassNotFoundException {
-        Reflection launcher = new Reflection();
+        ClassReaderCMD launcher = new ClassReaderCMD();
         launcher.launch();
     }
 
     /**
      * This method read the user input and evaluate if the Class exist, describe the name and package.
-     * @throws ClassNotFoundException
+     * @throws ClassNotFoundException when not found.
      */
     private void launch () throws ClassNotFoundException {
         Scanner reader = new Scanner(System.in);
         System.out.println("Enter a the class name: ");
         String n = reader.next();
         java.lang.Class.forName(n);// The try and catch is implicit on this line as the trows is implemented on the method.
-        System.out.println(n + " " + Class.forName(n).getName() + " " + Class.forName(n).getPackage());
+        System.out.println(n + " " + Class.forName(n).getName() + " " + Class.forName(n).getPackage()+ " "
+                + Class.forName(n).getInterfaces()+ " " + Class.forName(n).getModifiers()+ " " + Class.forName(n).getConstructors()
+                + " " + Class.forName(n).getFields());
 
     }
 }

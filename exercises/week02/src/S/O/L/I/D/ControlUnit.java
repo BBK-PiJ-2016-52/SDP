bb01acfc2b049199afd3188c610b05e424abf3b3
Eric Rodriguez <erodri07@mail.bbk.ac.uk>
2017-04-26 08:09:26 +0100

    Created Classes and Implemented interfaces per given specifications.

---
+++ exercises/week02/src/S/O/L/I/D/ControlUnit.java
@@ -0,0 +1,10 @@
+package S.O.L.I.D;
+
+/**
+ * Created by Eric on 24/01/2017.
+ */
+public class ControlUnit {
+    public static void main (String[] Args) {
+
+    }
+}
---
+++ exercises/week02/src/S/O/L/I/D/SmokeSensor.java
@@ -0,0 +1,27 @@
+package S.O.L.I.D;
+
+/**
+ * Created by Eric on 24/01/2017.
+ */
+public class SmokeSensor implements Sensor{
+
+    @Override
+    public boolean isTriggered() {
+        return false;
+    }
+
+    @Override
+    public String getLocation() {
+        return null;
+    }
+
+    @Override
+    public String getSensorType() {
+        return null;
+    }
+
+    @Override
+    public double getBatteryPercentage() {
+        return 0;
+    }
+}

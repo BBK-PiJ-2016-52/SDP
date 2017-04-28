fc34c75ad0b74cde3fa86a48f4b506c92c89cdb0
Eric Rodriguez <erodri07@mail.bbk.ac.uk>
2017-04-28 20:06:25 +0100

    Completed Exercise Week05 practice with Classes Question 3.

--- exercises/week05/src/main/scala/Question3/Director.scala
+++ exercises/week05/src/main/scala/Question3/Director.scala
@@ -1,6 +1,6 @@
 package Question3
 
-class Director (firstName : String ,lastName : String, yearOfBirth : Int ){
+case class Director (firstName : String ,lastName : String, yearOfBirth : Int ){
   def name (): String =  firstName + lastName
 }
 
@@ -8,4 +8,6 @@ object Director {
   def apply(firstName: String, lastName: String, yearOfBirth: Int): Director = {
     new Director(firstName, lastName, yearOfBirth)
   }
+  def older( d1: Director, d2: Director): Director = if (d1.yearOfBirth < d2.yearOfBirth) d1 else d2
+
 }
--- exercises/week05/src/main/scala/Question3/Film.scala
+++ exercises/week05/src/main/scala/Question3/Film.scala
@@ -1,10 +1,27 @@
 package Question3
 
-class Film(name : String, yearOfRelease : Int, imdbRating : Double, director : Director){
+case class Film(name : String, yearOfRelease : Int, imdbRating : Double, director : Director) {
 
   def directorsAge(): Int = yearOfRelease
 
-  def isDirectedBy(director: Director): Boolean = {
+  def isDirectedBy(d: Director): Boolean = {
+    if (director.equals(d)) true else false
+  }
+
+  def copy(newName: String, newYearOfRelease: Int, newImdbRating: Double, newDirector: Director): Film = {
+    new Film(newName, newYearOfRelease, newImdbRating, newDirector)
+  }
+}
+
+object Film {
+
+  def apply(name: String, yearOfRelease: Int, imdbRating: Double, director: Director): Film = {
+    new Film(name, yearOfRelease, imdbRating, director)
+  }
+  
+  def highestRating(film1: Film, film2: Film): Any ={
+    if (film1.imdbRating < film2.imdbRating) film2 else film1
   }
 
 }
+

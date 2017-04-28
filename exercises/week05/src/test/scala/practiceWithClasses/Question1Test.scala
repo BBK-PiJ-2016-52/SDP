176e7c1d385463f9df247f1be21a806fcfe0a623
Eric Rodriguez <erodri07@mail.bbk.ac.uk>
2017-04-28 17:29:09 +0100

    Completed Exercise Week05 practice with Classes.

--- exercises/week05/src/test/scala/practiceWithClasses/Question1Test.scala
+++ exercises/week05/src/test/scala/practiceWithClasses/Question1Test.scala
@@ -5,23 +5,23 @@ import org.scalatest.FunSuite
 class Question1Test  extends FunSuite {
 
   test("Expected assert increment") {
-    assert(new Counter(4).inc(4).count == 8)
+    assert(new Counter(4).inc(4) == 8)
   }
 
   test("Expected assert decrementing") {
-    assert(new Counter(6).dec(2).count == 4)
+    assert(new Counter(6).dec(2) == 4)
   }
 
   test("No parameter inc 1") {
-    assert(new Counter(1).inc().count == 6)
+    assert(new Counter(1).inc() == 2)
   }
 
   test("No parameter dec 1") {
-    assert(new Counter(5).dec().count == 4)
+    assert(new Counter(5).dec() == 4)
   }
 
   test("Adjust with an int adds the int to the counter") {
-    assert(new Counter(5).adjust(new Adder(3)).count == 8)
+    assert(new Counter(5).adjust(new Adder(3)) == 8)
   }
 
 }

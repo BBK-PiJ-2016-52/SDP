1e0dcd02d192342373d9d540646887548e9b2a40
Eric Rodriguez <erodri07@mail.bbk.ac.uk>
2017-04-29 02:09:10 +0100

    Completed AkkaNotes_Messaging

---
+++ exercises/week09/src/main/scala/AkkaNotes_Messaging/TeacherProtocol.scala
@@ -0,0 +1,6 @@
+object TeacherProtocol{
+
+  case class QuoteRequest()
+  case class QuoteResponse(quoteString:String)
+
+}
--- exercises/week09/src/main/scala/Singleton/Singleton.scala
+++
@@ -1,9 +0,0 @@
-object Fred
-class Fred private ()
-
-object Test extends App {
-  //println(new Singleton())
-  println(Fred)
-  //println(new Singleton())
-  println(Fred)
-}

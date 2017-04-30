7749305667540702941a01d7ceb06c1295adfb29
Eric Rodriguez <erodri07@mail.bbk.ac.uk>
2017-04-30 17:17:30 +0100

    Adding the subclass for printing

--- coursework/cw-one/src/main/scala/sml/OutInstruction.scala
+++ coursework/cw-one/src/main/scala/sml/OutInstruction.scala
@@ -1,8 +1,17 @@
 package sml
 
-/**
-  * Created by Eric on 05/04/2017.
-  */
-class OutInstruction {
+class OutInstruction(label: String, op: String, val op1: Int) extends Instruction(label, op) {
+  override def execute(m: Machine): Unit = {
+    val value = m.regs(op1)
+    m.regs(op1) = value
+  }
 
+  override def toString: String = {
+    super.toString + " " + op1
+  }
+}
+
+object OutInstruction {
+  def apply(label: String, op1: Int): OutInstruction =
+    new OutInstruction(label, "out", op1)
 }
